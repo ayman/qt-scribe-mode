8b193bb681eb3f4799be371531521184c02a5712
David A. Shamma <shamma@yahoo-inc.com>
2014-07-24 15:49:36 -0700

    Made a major mode for #1.

--- qt-scribe.el
+++ qt-scribe.el
@@ -22,13 +22,13 @@
 
 ;;; Commentary:
 
-;; All symbols use prefix qt-.
+;; All symbols use prefix qt-scribe.
 
 ;;; Code:
 
 (require 'cl-lib)
 
-(defun qt-format-timecode (sec)
+(defun qt-scribe-format-timecode (sec)
   (format "%02d:%02d:%02d.%02d" 
           (/ (floor sec) 3600)
           (/ (% (floor sec) 3600) 60)
@@ -36,43 +36,93 @@
           (floor (* (- sec (floor sec)) 100))
           (- (* sec 100) (* (floor sec) 100))))
 
-(defun qt-pause ()
+(defun qt-scribe-pause ()
   (interactive)
   (do-applescript
    (format "tell application \"QuickTime Player\"
 	        tell the front document to pause
             end tell")))
 
-(defun qt-play ()
+(defun qt-scribe-play ()
   (interactive)
   (do-applescript
    (format "tell application \"QuickTime Player\"
 	        tell the front document to play
             end tell")))
 
-(defun qt-get-filename ()
+(defun qt-scribe-toggle-playback ()
+  (interactive)
+  (do-applescript
+   (format "tell application \"QuickTime Player\"
+              if the front document is playing then
+                tell the front document to pause
+              else
+                tell the front document to play
+              end if
+            end tell")))
+
+(defun qt-scribe-get-filename ()
   (interactive)
   (insert
    (format 
-    "[file: '%s']\n"
+    "[file: \"%s\"]\n"
     (do-applescript
      (format "tell application \"QuickTime Player\"
                 the name of the front window
               end tell")))))
 
-(defun qt-get-time ()
+(defun qt-scribe-get-time ()
   (interactive)
   (insert
    (format 
     "[%s] "
-    (qt-format-timecode
+    (qt-scribe-format-timecode
      (string-to-number 
       (do-applescript
        (format "tell application \"QuickTime Player\"
   	          the current time of the front document as string
                 end tell")))))))
 
-(provide 'qt-scribe)
+;; command to comment/uncomment text
+(defun qt-scribe--comment-dwim (arg)
+  "Comment or uncomment current line or region in a smart way.
+For detail, see `comment-dwim'."
+  (interactive "*P")
+  (require 'newcomment)
+  (let (
+        (comment-start "#") (comment-end "")
+        )
+    (comment-dwim arg)))
+
+
+(defconst qt-scribe-types '("file:" "time:"))
+(defconst qt-scribe-type-regexp (regexp-opt qt-scribe-types 'words))
+(setq qt-scribe-font-lock-keywords
+      `(
+        (,qt-scribe-type-regexp . font-lock-type-face)
+        ;;(,qt-scribe-constant-regexp . font-lock-constant-face)
+        ;;(,qt-scribe-event-regexp . font-lock-builtin-face)
+        ;;(,qt-scribe-functions-regexp . font-lock-function-name-face)
+        ;;(,qt-scribe-keywords-regexp . font-lock-keyword-face)
+        ))
+
+(defvar qt-scribe-mode-hook nil)
+
+(defvar qt-scribe-mode-map
+  (let ((keymap (make-sparse-keymap)))
+    (define-key keymap (kbd "C-c RET") 'qt-scribe-get-time)
+    (define-key keymap (kbd "C-c j") 'qt-scribe-toggle-playback)
+    keymap)
+  "Keymap for qt-scribe major mode")
+
+(define-derived-mode qt-scribe-mode fundamental-mode
+  "qt-scribe mode"
+  "Major mode for transcribing using the Quicktime Player on Mac OS."
+  (setq mode-name "qt-scribe")
+  (setq font-lock-defaults '((qt-scribe-font-lock-keywords)))
+  (define-key qt-scribe-mode-map [remap comment-dwim] 'qt-scribe-comment-dwim))
+
+(provide 'qt-scribe-mode)
 
 ;;; qt-scribe.el ends here
 
